d09a40a5ac9c29071cc0f220154c9355a160493d
JannoSiim <jannosiim@gmail.com>
2025-01-17 16:49:50 +0000

    Deploying to gh-pages from @ JannoSiim/jannosiim.github.io@ca763c6758d0e0853a549a50634789b800a9e9b7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,16 +431,8 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+    },{id: "news-i-am-starting-as-a-lecturer-in-cryptography-in-the-university-of-tartu",
+          title: 'I am starting as a lecturer in cryptography in the University of Tartu!...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
